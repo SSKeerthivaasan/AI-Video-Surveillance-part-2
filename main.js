cbe0575dce384e48a7210663090a46caf677e211
SSKeerthivaasan <91375660+SSKeerthivaasan@users.noreply.github.com>
2022-09-27 19:40:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+video="";
+Status="";
+function setup(){
+    canvas=createCanvas(480,380);
+    canvas.center();
+    video.hide();
+}
+function draw(){
+image(video,0,0,480,380);
+}
+function preload(){
+    video=createVideo("video.mp4");
+    
+}
+function start(){
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML=" Status : Detecting Objects";
+}
+function modelLoaded(){
+    console.log("model Loaded");
+    Status=true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
